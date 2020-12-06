34f961c1264e9b3572d940f7634f731063a41ec6
Blarc <jakob.malezic@gmail.com>
2020-12-06 01:35:30 +0100

    bongacams fix

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def juric(ctx):
 async def bongacams(ctx):
     with open('text/bongacams.json') as json_file:
         data = json.load(json_file)
-        for title, link in data:
+        for title, link in data.values():
             await ctx.send(f"{title}: {link}")
 
 
